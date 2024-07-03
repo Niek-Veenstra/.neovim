7f461b9fec8c5bce9021dc4589cbc2bc375a95b9
Niek Veenstra <537008@student.saxion.nl>
2024-07-04 00:01:51 +0200

    Updated the config.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+map("n", "<leader>RPY", ":w<CR>:TermExec cmd='python3 %'<CR>", { noremap = true, silent = true })
+map("n", "<leader>fl", ":Telescope live_grep<CR>", { noremap = true, silent = true })
