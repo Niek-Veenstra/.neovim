f7da2f24ac023c96cf09233de34009e4ab24657d
Niek Veenstra <537008@student.saxion.nl>
2024-07-21 22:59:36 +0200

    Fixed some stuff.

--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -6,12 +6,5 @@ return (new Config())->setRules(
     [
         '@PSR12' => true,
         '@Symfony' => true,
-        //   'strict_param' => true,
-        //   'array_syntax' => ['syntax' => 'short'],
-        //   'indentation_type' => true, // Ensures spaces or tabs are used consistently
-        // 'braces' => ['position_after_functions_and_oop_constructs' => 'same'],
-        // 'array_syntax' => ['syntax' => 'short'],
-        //   'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
-        // 'indent' => 4
     ]
 );
