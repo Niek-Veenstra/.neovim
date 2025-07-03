df240ddff3a463b023c3b1e8fab9e6407ba083f5
Niek Veenstra <niekveenstra14@gmail.com>
2025-07-03 18:50:23 +0200

    New LSP setup.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,29 +2,28 @@ return {
   "saghen/blink.cmp",
   dependencies = "rafamadriz/friendly-snippets",
   version = "*",
+  config = function()
+    require("blink.cmp").setup({
+      keymap = {
+        preset = "super-tab",
+        ["<C-j>"] = { "scroll_documentation_down" },
+        ["<C-k>"] = { "scroll_documentation_up", "fallback" },
+      },
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = "super-tab",
-      ["<C-j>"] = { "scroll_documentation_down" },
-      ["<C-k>"] = { "scroll_documentation_up", "fallback" },
-    },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        trigger = {
+          show_on_trigger_character = true,
+        },
+      },
 
-    appearance = {
-      use_nvim_cmp_as_default = false,
-      nerd_font_variant = "mono",
-    },
-    completion = {
-      trigger = {
-        show_on_trigger_character = true,
-      }
-    },
-
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-  },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    })
+  end,
   opts_extend = { "sources.default" },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,57 +9,90 @@ return {
   },
   opts = {},
   config = function()
-    function set_key_binds(opts)
-      local keymap = vim.keymap
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.get_prev_pos, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.get_next_pos, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-    end
-
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
     local utilities = require("lspconfig.util")
     local esp32 = require("esp32")
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "html",
+        "cssls",
+        "svelte",
+        "lua_ls",
+        "vtsls",
+        "pyright",
+        "clangd",
+        "intelephense",
+        "emmet_language_server",
+        "ts_ls",
+        "tailwindcss",
+        "volar",
+        "eslint",
+      },
+      automatic_installation = {
+        "html",
+        "cssls",
+        "svelte",
+        "lua_ls",
+        "pyright",
+        "clangd",
+        "intelephense",
+      },
+    })
 
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      group = vim.api.nvim_create_augroup("lsp-attach", {}),
       callback = function(ev)
+        local keymap = vim.keymap
         local opts = { buffer = ev.buf, silent = true }
-        set_key_binds(opts)
-        opts.desc = "Show LSP references"
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+        opts.desc = "Go to declaration"
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+        opts.desc = "Show LSP definitions"
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+        opts.desc = "Show LSP implementations"
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+        opts.desc = "Show LSP type definitions"
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+        opts.desc = "See available code actions"
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+        opts.desc = "Smart rename"
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+        opts.desc = "Show buffer diagnostics"
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+        opts.desc = "Show line diagnostics"
+        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+        opts.desc = "Go to previous diagnostic"
+        keymap.set("n", "[d", vim.diagnostic.get_prev_pos, opts) -- jump to previous diagnostic in buffer
+
+        opts.desc = "Go to next diagnostic"
+        keymap.set("n", "]d", vim.diagnostic.get_next_pos, opts) -- jump to next diagnostic in buffer
+
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+        opts.desc = "Restart LSP"
+        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
       end,
     })
 
@@ -70,19 +103,34 @@ return {
     end
     local vue_language_server_path = vim.fn.stdpath("data")
       .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-    local vue_plugin = {
-      name = "@vue/typescript-plugin",
-      location = vue_language_server_path,
-      languages = { "vue" },
-      configNamespace = "typescript",
-    }
     local vtsls_config = {
-      init_options = {
-        plugins = {
-          vue_plugin,
+      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      settings = {
+        vtsls = { tsserver = { globalPlugins = {} } },
+        typescript = {
+          inlayHints = {
+            parameterNames = { enabled = "literals" },
+            parameterTypes = { enabled = true },
+            variableTypes = { enabled = true },
+            propertyDeclarationTypes = { enabled = true },
+            functionLikeReturnTypes = { enabled = true },
+            enumMemberValues = { enabled = true },
+          },
         },
       },
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      before_init = function(_, config)
+        table.insert(config.settings.vtsls.tsserver.globalPlugins, {
+          name = "@vue/typescript-plugin",
+          location = vim.fn.expand(vue_language_server_path),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        })
+      end,
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
     }
     local config_svelte = {
       on_attach = function(client)
@@ -95,12 +143,10 @@ return {
       end,
     }
     local config_tailwindcss = {
-      capabilities = capabilities,
       root_dir = utilities.root_pattern(".git", "package.json", "package-lock.json", "tsconfig.json", "jsconfig.json"),
     }
 
     local config_graphql = {
-      capabilities = capabilities,
       filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
     }
 
@@ -141,7 +187,6 @@ return {
     }
 
     local config_lua_ls = {
-      capabilities = capabilities,
       settings = {
         Lua = {
           diagnostics = {
@@ -155,7 +200,6 @@ return {
     }
 
     local config_intelephense = {
-      capabilities = capabilities,
       filetypes = { "php", "phtml" },
       root_dir = utilities.root_pattern(".git", ".exercism"),
       files = {
@@ -164,13 +208,11 @@ return {
     }
 
     local config_html = {
-      capabilities = capabilities,
       filetypes = { "php", "phtml", "html" },
       root_dir = utilities.root_pattern(".git"),
     }
 
-    local config_clangd = esp32.lsp_config()({
-      capabilities = capabilities,
+    local config_clangd = esp32.lsp_config({
       cmd = {
         "clangd",
         "--log=verbose",
@@ -190,7 +232,6 @@ return {
     })
 
     local config_vhdl_ls = {
-      capabilities = capabilities,
       filetypes = { "vhdl" },
       root_dir = utilities.root_pattern("clash-manifest.json"),
     }
@@ -214,6 +255,5 @@ return {
     vim.lsp.config("graphql", config_graphql)
     vim.lsp.config("tailwindcss", config_tailwindcss)
     vim.lsp.config("svelte", config_svelte)
-    vim.lsp.enable({ "vtsls", "vue_ls" })
   end,
 }
