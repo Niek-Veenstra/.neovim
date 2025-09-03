ccde5a890d3235fc08d4da9e1e0dec19cf1b704a
Niek Veenstra <niekveenstra14@gmail.com>
2025-09-03 20:07:53 +0200

    Added rustacean and treesitter.

---
+++ lua/plugins/rustacean.lua
@@ -0,0 +1,19 @@
+return {
+  "mrcjkb/rustaceanvim",
+  version = "^6",
+  lazy = false,
+  config = function()
+    -- vim.cmd.RustAnalyzer({
+    --   "config",
+    --   {
+    --     check = {
+    --       command = "clippy",
+    --     },
+    --     diagnostics = {
+    --       enable = true,
+    --       enableExperimental = true,
+    --     },
+    --   },
+    -- })
+  end,
+}
