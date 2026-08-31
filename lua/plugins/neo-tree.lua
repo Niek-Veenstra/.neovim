4c37846e60944b0e227ef0edeed39934932e3d4d
niekv <n.veenstra@msquad.nl>
2026-08-31 15:34:08 +0200

    Add new styling functionality.

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,130 @@
+return {
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      default = {
+        directory = { glyph = "󰉋", hl = "MiniIconsAzure" },
+      },
+      directory = {
+        [".git"] = { glyph = "", hl = "MiniIconsOrange" },
+        [".github"] = { glyph = "", hl = "MiniIconsAzure" },
+        [".gitlab"] = { glyph = "", hl = "MiniIconsOrange" },
+        [".idea"] = { glyph = "", hl = "MiniIconsPurple" },
+        [".vscode"] = { glyph = "󰨞", hl = "MiniIconsAzure" },
+        [".husky"] = { glyph = "󰤂", hl = "MiniIconsYellow" },
+        [".config"] = { glyph = "", hl = "MiniIconsCyan" },
+        [".cache"] = { glyph = "󰪺", hl = "MiniIconsGrey" },
+        src = { glyph = "󰣞", hl = "MiniIconsPurple" },
+        source = { glyph = "󰣞", hl = "MiniIconsPurple" },
+        sources = { glyph = "󰣞", hl = "MiniIconsPurple" },
+        lua = { glyph = "", hl = "MiniIconsBlue" },
+        nvim = { glyph = "", hl = "MiniIconsGreen" },
+        app = { glyph = "󰀻", hl = "MiniIconsAzure" },
+        apps = { glyph = "󰀻", hl = "MiniIconsAzure" },
+        api = { glyph = "󰒍", hl = "MiniIconsGreen" },
+        backend = { glyph = "󰒋", hl = "MiniIconsOrange" },
+        frontend = { glyph = "󰖟", hl = "MiniIconsAzure" },
+        client = { glyph = "󰖟", hl = "MiniIconsCyan" },
+        server = { glyph = "󰒋", hl = "MiniIconsOrange" },
+        components = { glyph = "󰡀", hl = "MiniIconsRed" },
+        hooks = { glyph = "󰛢", hl = "MiniIconsCyan" },
+        lib = { glyph = "󰲂", hl = "MiniIconsYellow" },
+        libs = { glyph = "󰲂", hl = "MiniIconsYellow" },
+        utils = { glyph = "󰒓", hl = "MiniIconsGrey" },
+        helpers = { glyph = "󰒓", hl = "MiniIconsGrey" },
+        scripts = { glyph = "󰆍", hl = "MiniIconsYellow" },
+        bin = { glyph = "󱧺", hl = "MiniIconsYellow" },
+        include = { glyph = "󰋺", hl = "MiniIconsPurple" },
+        vendor = { glyph = "󰏗", hl = "MiniIconsOrange" },
+        packages = { glyph = "󰏗", hl = "MiniIconsOrange" },
+        node_modules = { glyph = "", hl = "MiniIconsGreen" },
+        test = { glyph = "󰙨", hl = "MiniIconsGreen" },
+        tests = { glyph = "󰙨", hl = "MiniIconsGreen" },
+        spec = { glyph = "󰙨", hl = "MiniIconsGreen" },
+        ["__tests__"] = { glyph = "󰙨", hl = "MiniIconsGreen" },
+        e2e = { glyph = "󰙨", hl = "MiniIconsAzure" },
+        cypress = { glyph = "󰯲", hl = "MiniIconsGreen" },
+        docs = { glyph = "󱂷", hl = "MiniIconsPurple" },
+        doc = { glyph = "󱂷", hl = "MiniIconsPurple" },
+        documentation = { glyph = "󱂷", hl = "MiniIconsPurple" },
+        config = { glyph = "", hl = "MiniIconsCyan" },
+        configs = { glyph = "", hl = "MiniIconsCyan" },
+        build = { glyph = "󰣪", hl = "MiniIconsGrey" },
+        dist = { glyph = "󰣞", hl = "MiniIconsGrey" },
+        out = { glyph = "󰣞", hl = "MiniIconsGrey" },
+        public = { glyph = "󰖟", hl = "MiniIconsGreen" },
+        static = { glyph = "󰖟", hl = "MiniIconsGreen" },
+        assets = { glyph = "󰉏", hl = "MiniIconsYellow" },
+        images = { glyph = "󰉏", hl = "MiniIconsYellow" },
+        img = { glyph = "󰉏", hl = "MiniIconsYellow" },
+        icons = { glyph = "󰉏", hl = "MiniIconsAzure" },
+        fonts = { glyph = "", hl = "MiniIconsRed" },
+        css = { glyph = "", hl = "MiniIconsAzure" },
+        styles = { glyph = "", hl = "MiniIconsAzure" },
+        scss = { glyph = "", hl = "MiniIconsRed" },
+        locales = { glyph = "󰗊", hl = "MiniIconsGreen" },
+        i18n = { glyph = "󰗊", hl = "MiniIconsGreen" },
+        data = { glyph = "󰆼", hl = "MiniIconsYellow" },
+        db = { glyph = "󰆼", hl = "MiniIconsYellow" },
+        docker = { glyph = "󰡨", hl = "MiniIconsAzure" },
+        android = { glyph = "", hl = "MiniIconsGreen" },
+        ios = { glyph = "", hl = "MiniIconsGrey" },
+        plugins = { glyph = "󰐱", hl = "MiniIconsPurple" },
+        plugin = { glyph = "󰐱", hl = "MiniIconsPurple" },
+        themes = { glyph = "󰔎", hl = "MiniIconsRed" },
+        pages = { glyph = "󰈙", hl = "MiniIconsOrange" },
+        views = { glyph = "󰈙", hl = "MiniIconsOrange" },
+        layouts = { glyph = "󰕮", hl = "MiniIconsCyan" },
+        routes = { glyph = "󰑪", hl = "MiniIconsGreen" },
+        types = { glyph = "󰋄", hl = "MiniIconsAzure" },
+        tmp = { glyph = "󰪺", hl = "MiniIconsGrey" },
+        temp = { glyph = "󰪺", hl = "MiniIconsGrey" },
+        logs = { glyph = "󰷈", hl = "MiniIconsGrey" },
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        icon = {
+          folder_closed = "󰉋",
+          folder_open = "󰝰",
+          folder_empty = "󰉖",
+          folder_empty_open = "󰷏",
+          provider = function(icon, node)
+            if node.type == "file" or node.type == "terminal" then
+              local ok, devicons = pcall(require, "nvim-web-devicons")
+              if ok then
+                local name = node.type == "terminal" and "terminal" or node.name
+                local devicon, hl = devicons.get_icon(name)
+                if devicon then
+                  icon.text = devicon
+                end
+                if hl then
+                  icon.highlight = hl
+                end
+              end
+              return
+            end
+
+            if node.type ~= "directory" then
+              return
+            end
+
+            local text, hl, is_default = require("mini.icons").get("directory", node.name)
+            if node:is_expanded() and is_default then
+              return
+            end
+            if text then
+              icon.text = text
+            end
+            if hl then
+              icon.highlight = hl
+            end
+          end,
+        },
+      },
+    },
+  },
+}
